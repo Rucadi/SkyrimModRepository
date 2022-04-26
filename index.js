20b117d76fe94ac427016d102ce056a576bdbf34
rucadi <ruben.cano96@gmail.com>
2022-04-26 21:44:55 +0200

    remove hack for testing multiple elements

--- index.js
+++ index.js
@@ -261,7 +261,7 @@ function loadModView (REPO_FILTERED) {
   const loadMore = function () {
     
     for (let i = 0; i < 20 && REPO_FILTERED.length >= 1; i++) {
-      var mod = REPO_FILTERED[0];
+      var mod = REPO_FILTERED.pop();
         const imageIdentifier = "image-"+mod.ImagePreview;
 
         const item = document.createElement('div')
